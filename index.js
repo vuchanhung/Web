1fde2649be6c71174d3dc924952a7ef717e19486
Hung Vu <93482923+vuchanhung@users.noreply.github.com>
2022-10-26 13:54:29 +0700

    aloo

---
+++ index.js
@@ -0,0 +1,27 @@
+var profile=document.getElementById('profile');
+
+profile.onclick=function(){
+    window.location="#profile-position";
+};
+
+
+var experience=document.getElementById('experience');
+
+experience.onclick=function(){
+    window.location="#experience-position";
+
+};
+
+var skills=document.getElementById('skills');
+
+skills.onclick=function(){
+    window.location="#skills-position";
+};
+
+
+var languages=document.getElementById('languages');
+
+languages.onclick=function(){
+    window.location="#languages-position";
+};
+
